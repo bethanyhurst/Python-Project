e403892e52d545fd1d1eac1d512ba21e3b12e84a
Haydnmiller <48588780+Haydnmiller@users.noreply.github.com>
2019-04-10 23:31:44 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -2,13 +2,16 @@ import random
 import string
 
 
-for k in range(0, 11):
+for k in range(0, 11): #Choose a cipher
     number = random.randint(0, 1)
     if number == 0:
 
 def caeser():
-    number = random.randint(0, 25)
-    generateQuestion = random.randint(0, 1)
-    alphabetList = list(string.ascii_uppercase)
-    if generateQuestion == 0:
+    number = random.randint(0, 25) #Choose a shift
+    generateQuestion = random.randint(0, 1) #Choose encode or decode
+    alphabetList = list(string.ascii_uppercase) #Put the alphabet into a list
+    if generateQuestion == 0: #Encode
+    
+    else: #Decode
+        
 
