f03297f5d3c99a89dd1f97d6ba3b94e61b8ec51d
KatieNaretti <47121493+KatieNaretti@users.noreply.github.com>
2019-04-10 23:28:58 +0100

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+
+for k in range(0, 11):
+    number = random.randint(0, 1)
+    if number == 0:
+
+def caeser():
+    number = random.randint(0, 25)
+    generateQuestion = random.randint(0, 1)
+    alphabetList = list(string.ascii_uppercase)
+    if generateQuestion == 0:
+
